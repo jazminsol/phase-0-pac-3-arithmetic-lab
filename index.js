715fded85639df1ee5f3b16173fbcd589a3da372
jazmin <jazminsol@yahoo.com>
2021-03-25 22:17:49 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,31 @@
+function add () {`+`}
+function subtract () {`-`}
+function multiply () {`*`}
+function divide () {`/`}
+
+function add (a, b){
+    return a + b
+}
+function subtract (b, c) {
+    return b - c
+}
+function multiply (d, f) {
+    return d * f
+}
+function divide (e, f) {
+    return e / f
+}
+function increment(n){
+    return n + 1
+}
+function decrement(m){
+    return m - 1
+}
+function makeInt(n) {
+    parseInt(n, 10)
+    return parseInt(n, 10)
+
+}
+function preserveDecimal(h) {
+    return parseFloat(h)
+}
